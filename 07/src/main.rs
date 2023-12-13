e49448dec36e007713cb0284ed287825f05fada0
Pieter Scheffers <pieter.scheffers@gmail.com>
2023-12-13 13:12:42 +0100

    feat(07): Part one finished

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering::{Equal, Greater, Less};
+use std::collections::HashMap;
 use std::convert::TryInto;
 use std::fs::read_to_string;
 
@@ -12,7 +14,7 @@ fn main() {
     .trim()
     .to_string();
 
-    // let input = read_file("input.txt");
+    let input = read_file("input.txt");
 
     let result_part_one = part_one(&input);
     println!("result_part_one: {}", result_part_one);
@@ -73,7 +75,7 @@ fn vector_to_array<T, const N: usize>(v: Vec<T>) -> [T; N] {
         .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
 }
 
-fn map_cards_to_type(cards: [i64; 5]) -> i64 {
+fn map_cards_to_type(cards: &[i64; 5]) -> i64 {
     // 7: Five of a kind, where all five cards have the same label: AAAAA
     // 6: Four of a kind, where four cards have the same label and one card has a different label: AA8AA
     // 5: Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
@@ -82,16 +84,109 @@ fn map_cards_to_type(cards: [i64; 5]) -> i64 {
     // 2: One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
     // 1: High card, where all cards' labels are distinct: 23456
 
-    // TODO
+    let grouped: HashMap<i64, i64> = cards.iter().fold(HashMap::new(), |mut acc, c| {
+        let def: i64 = 0;
+        let i: i64 = *acc.get(c).unwrap_or(&def);
+        acc.insert(*c, i + 1);
+        acc
+    });
+
+    // println!("grouped: {:?}", grouped);
+
+    let def: (i64, i64) = (0, 0);
+    let longest = (&grouped).into_iter().fold(def, |acc, card| {
+        if *card.1 > acc.1 {
+            (*card.0, *card.1)
+        } else {
+            acc
+        }
+    });
+
+    // println!("longest: {:?}", longest);
+
+    let second_longest = grouped
+        .into_iter()
+        .filter(|(card, _)| card != &longest.0)
+        .fold(def, |acc, card| {
+            if card.1 > acc.1 {
+                (card.0, card.1)
+            } else {
+                acc
+            }
+        });
+
+    // println!("second_longest: {:?}", second_longest);
+
+    if longest.1 == 5 {
+        return 7;
+    } else if longest.1 == 4 {
+        return 6;
+    } else if longest.1 == 3 && second_longest.1 == 2 {
+        return 5;
+    } else if longest.1 == 3 {
+        return 4;
+    } else if longest.1 == 2 && second_longest.1 == 2 {
+        return 3;
+    } else if longest.1 == 2 {
+        return 2;
+    }
 
     1
 }
 
-fn part_one(raw_input: &String) -> i64 {
-    let sets = parse_input(raw_input);
+fn sort_sets(sets: &mut Vec<([i64; 5], i64)>) -> &mut Vec<([i64; 5], i64)> {
+    let hand_type_cmp = |l: &([i64; 5], i64), r: &([i64; 5], i64)| {
+        map_cards_to_type(&l.0).cmp(&map_cards_to_type(&r.0))
+    };
 
-    println!("sets: {:?}", sets);
-    2
+    let first_cmp = |l: &([i64; 5], i64), r: &([i64; 5], i64)| l.0[0].cmp(&r.0[0]);
+    let second_cmp = |l: &([i64; 5], i64), r: &([i64; 5], i64)| l.0[1].cmp(&r.0[1]);
+    let third_cmp = |l: &([i64; 5], i64), r: &([i64; 5], i64)| l.0[2].cmp(&r.0[2]);
+    let fourth_cmp = |l: &([i64; 5], i64), r: &([i64; 5], i64)| l.0[3].cmp(&r.0[3]);
+    let fifth_cmp = |l: &([i64; 5], i64), r: &([i64; 5], i64)| l.0[4].cmp(&r.0[4]);
+
+    sets.sort_by(|a, b| {
+        let result = hand_type_cmp(&a, &b);
+        if result != Equal {
+            return result;
+        }
+
+        let result = first_cmp(&a, &b);
+        if result != Equal {
+            return result;
+        }
+
+        let result = second_cmp(&a, &b);
+        if result != Equal {
+            return result;
+        }
+
+        let result = third_cmp(&a, &b);
+        if result != Equal {
+            return result;
+        }
+
+        let result = fourth_cmp(&a, &b);
+        if result != Equal {
+            return result;
+        }
+
+        fifth_cmp(&a, &b)
+    });
+
+    sets
+}
+
+fn part_one(raw_input: &String) -> i64 {
+    let mut sets: Vec<([i64; 5], i64)> = parse_input(raw_input);
+    // println!("sets: {:?}", sets);
+
+    let sets = sort_sets(&mut sets);
+    // println!("sets: {:?}", sets);
+
+    sets.iter().enumerate().fold(0, |acc, (index, (_, bid))| {
+        acc + ((i64::try_from(index).expect("Cannot convert usize to i64") + 1) * bid)
+    })
 }
 
 fn part_two(raw_input: &String) -> i64 {
@@ -116,5 +211,16 @@ mod tests {
 
         let result = part_one(&input);
         assert_eq!(result, 6440);
+    }
+
+    #[test]
+    fn can_map_cards_to_type() {
+        assert_eq!(map_cards_to_type(&[5, 2, 10, 3, 13]), 1);
+        assert_eq!(map_cards_to_type(&[3, 2, 10, 3, 13]), 2);
+        assert_eq!(map_cards_to_type(&[13, 13, 6, 7, 7]), 3);
+        assert_eq!(map_cards_to_type(&[10, 5, 5, 11, 5]), 4);
+        assert_eq!(map_cards_to_type(&[3, 10, 10, 3, 3]), 5);
+        assert_eq!(map_cards_to_type(&[3, 3, 10, 3, 3]), 6);
+        assert_eq!(map_cards_to_type(&[3, 3, 3, 3, 3]), 7);
     }
 }
